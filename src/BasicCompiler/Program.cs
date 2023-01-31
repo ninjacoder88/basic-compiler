0fd6b3c173e354008f12581cfc435858bc06d09b
Tom Harris <tomharris88@outlook.com>
2023-01-30 20:19:16 -0600

    init

---
+++ src/BasicCompiler/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter file path");
+string? filePath = Console.ReadLine();
+
+if(string.IsNullOrEmpty(filePath))
+{
+    Console.WriteLine("invalid file");
+    return;
+}
+
+if(!File.Exists(filePath))
+{
+    Console.WriteLine("invalid file path");
+    return;
+}
+
+using(var streamReader = new StreamReader(filePath))
+{
+    while(!streamReader.EndOfStream)
+    {
+        string? fileLine = streamReader.ReadLine();
+
+        if(string.IsNullOrEmpty(fileLine))
+        {
+            continue;
+        }
+
+        foreach(var character in fileLine)
+        {
+            
+        }
+    }
+}
